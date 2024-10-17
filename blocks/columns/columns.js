4d9415c3a3e5b813a3147f027b33db119932bcc7
sonhongle <sonpro7733@gmail.com>
2024-10-17 10:50:06 +0700

    2nd edit

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,28 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  const headings = document.querySelectorAll('.columns > div > div > h2');
+  headings.forEach((heading) => {
+    heading.classList.add('columns-heading');
+  });
+
+  const paragraphs = document.querySelectorAll('.columns > div > div > p');
+  paragraphs.forEach((paragraph) => {
+    paragraph.classList.add('columns-paragraph');
+  });
+
+  const divs = document.querySelectorAll('.columns > div > div');
+  divs.forEach((div) => {
+  div.classList.add('products');
+  });
+
+  const buttonLinks = document.querySelectorAll('p em a');
+
+  buttonLinks.forEach(buttonLink => {
+  buttonLink.classList.add('button-secondary');
+  });
+
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
