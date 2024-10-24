93237bc0ea068bc2c4fbeda31a18ed7675d39f41
sonhongle <147123703+sonhongle@users.noreply.github.com>
2024-10-24 14:11:46 +0700

    Feature/update column block styles (#3)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,43 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  const headings = document.querySelectorAll('.columns > div > div > h2');
+  headings.forEach((heading) => {
+    heading.classList.add('columns-heading');
+  });
+
+  const paragraphs = document.querySelectorAll('.columns > div > div > p');
+  paragraphs.forEach((paragraph) => {
+    paragraph.classList.add('columns-paragraph');
+  });
+
+  const divs = document.querySelectorAll('.columns > div > div');
+  divs.forEach((div) => {
+  div.classList.add('products');
+  });
+
+  const buttonLinks = document.querySelectorAll('p em a');
+
+  buttonLinks.forEach(buttonLink => {
+  buttonLink.classList.add('button-secondary');
+  });
+
+const productElements = document.querySelectorAll(".products");
+
+productElements.forEach(product => {
+  const firstChild = document.createElement("div");
+  firstChild.classList.add("psale");
+  firstChild.textContent = "Bán chạy";
+
+  const secondChild = document.createElement("div");
+  secondChild.classList.add("sale-off");
+  secondChild.textContent = "-23%";
+
+  product.appendChild(firstChild);
+  product.appendChild(secondChild);
+});
+
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
